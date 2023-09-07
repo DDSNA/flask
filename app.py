dfafbfafd8b4f9190a473119c85ef87e9ccf8bd7
Dan <79444147+DDSNA@users.noreply.github.com>
2023-09-07 12:47:04 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,10 @@ import os.path
 from datetime import datetime
 
 import requests
-from flask import Flask, render_template, current_app, redirect
-from sqlalchemy import create_engine, insert, MetaData, Table
+from flask import Flask, render_template, current_app, redirect, jsonify
+from flask_caching import Cache
+from sqlalchemy import create_engine, insert, MetaData, Table, select
+from sqlalchemy.orm import sessionmaker
 
 # security import
 
@@ -14,11 +16,20 @@ app = Flask(__name__)
 env_config = os.getenv("APP_SETTINGS", "config.DevelopmentConfig")
 app.config.from_object(env_config)
 
-# actual security
+# config
+
+config = {
+    "DEBUG": True,          # some Flask specific configs
+    "CACHE_TYPE": "SimpleCache",  # Flask-Caching related configs
+    "CACHE_DEFAULT_TIMEOUT": 900
+}
+
+app.config.from_mapping(config)
+cache = Cache(app)
 
 
 # only creating this here because of security reasons
-connection_string = "mysql+mysqldb://pythonapp:Pythonsisaorma2023@containers-us-west-174.railway.app:7822/railway"
+connection_string = "mysql+mysqldb://pythonapp:MareParolaMare98@containers-us-west-174.railway.app:7822/restricted_schema"
 engine = create_engine(connection_string, echo=True)
 
 # logging
@@ -31,6 +42,7 @@ logging.basicConfig()
 
 
 @app.route('/', methods=['GET'])
+@cache.cached(timeout=1000)
 def index():
     try:
         render_template('index.html')
@@ -46,8 +58,10 @@ def secret_key():
     return f"The configured secret key is {__secret_key}."
 
 
-@app.route('/call-fuzz', methods=['GET'])
+@app.route('/call-fuzz')
+@cache.cached(timeout=600)
 def call_fuzz():
+
     api_url = (
         "https://market.fuzzwork.co.uk/aggregates/?region=60008494&types=2268,2305,2267,2288,2287,2307,2272,2309,2073,2310,2270,2306,2286,2311,2308,2393,2396,3779,2401,2390,2397,2392,3683,2389,2399,2395,2398,9828,2400,3645,2329,3828,9836,9832,44,3693,15317,3725,3689,2327,9842,2463,2317,2321,3695,9830,3697,9838,2312,3691,2319,9840,3775,2328,2358,2345,2344,2367,17392,2348,9834,2366,2361,17898,2360,2354,2352,9846,9848,2351,2349,2346,12836,17136,28974,2867,2868,2869,2870,2871,2872,2875,2876%22")
     response = requests.get(api_url)
@@ -61,13 +75,15 @@ def call_fuzz():
     return thedata
 
 
-@app.route('/call-to-db', methods=['GET'])
+@app.route('/call-to-db')
+@cache.cached(timeout=100)
 def call_db_and_fuzz():
     api_url = (
         "https://market.fuzzwork.co.uk/aggregates/?region=60008494&types=2268,2305,2267,2288,2287,2307,2272,2309,2073,2310,2270,2306,2286,2311,2308,2393,2396,3779,2401,2390,2397,2392,3683,2389,2399,2395,2398,9828,2400,3645,2329,3828,9836,9832,44,3693,15317,3725,3689,2327,9842,2463,2317,2321,3695,9830,3697,9838,2312,3691,2319,9840,3775,2328,2358,2345,2344,2367,17392,2348,9834,2366,2361,17898,2360,2354,2352,9846,9848,2351,2349,2346,12836,17136,28974,2867,2868,2869,2870,2871,2872,2875,2876")
     response = requests.get(api_url)
 
     data = response.text
+    # noinspection SpellCheckingInspection
     thedata = json.loads(data)
     print(response.status_code)
 
@@ -101,7 +117,7 @@ def call_db_and_fuzz():
             conn.commit()
             print(read_test)
         except Exception as e:
-            print("An error occurred during database insertion:", e)
+            print("An error occurred during database insertion: ", e)
             import traceback
             traceback.print_exc()
 
@@ -112,6 +128,37 @@ def call_db_and_fuzz():
     return thedata
 
 
+@app.route('/historical_table')
+@cache.cached(timeout=60)
+def show_historical_jsons():
+    connection_string_view = "mysql+mysqldb://frontend_tabledisplay:MareParolaMare98@containers-us-west-174.railway.app:7822/restricted_schema"
+    engine_2 = create_engine(connection_string_view, echo=True)
+    metadata = MetaData()
+    Session = sessionmaker(bind=engine_2)
+    session = Session()
+    view_table = Table("json_table_backup", metadata, autoload_with=engine_2)
+    with engine_2.connect() as conn_2:
+        get_view_data = select(view_table)
+        print(get_view_data)
+        try:
+            _results = session.execute(get_view_data)
+            _rows = _results.fetchall()
+            _results_dictionary = [{'data': json.dumps(row[0]), 'timestamp': row[1]} for row in _rows]
+            conn_2.close()
+            try:
+                render_template('historical_datapage.html')
+            except Exception as e:
+                return ('Got this error lol:', e)
+        except Exception as e:
+            print("Whoa! Error! This is th error itself: ", e)
+            import traceback
+            traceback.print_exc()
+            conn_2.close()
+        return jsonify(_results_dictionary)
+
+
+
+# redirect for mismatch between / and /static
 @app.route('/static/')
 def static_redirect():
     return redirect('/', code=302)
