d477dbfe98a28e2e0c572abc9644af4291f9639c
Dan <79444147+DDSNA@users.noreply.github.com>
2023-09-08 10:34:40 +0300

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ cache = flask_caching.Cache(app)
 
 # only creating this here because of security reasons
 connection_string = "mysql+mysqldb://pythonapp:MareParolaMare98@containers-us-west-174.railway.app:7822/restricted_schema"
-engine = create_engine(connection_string, echo=True)
+engine = create_engine(connection_string, pool_recycle=600, echo=True)
 
 # logging
 logging.basicConfig()
@@ -78,7 +78,7 @@ def call_fuzz():
 
 
 @app.route('/call-to-db')
-@cache.cached(timeout=100)
+@cache.cached(timeout=200)
 def call_db_and_fuzz():
     api_url = (
         "https://market.fuzzwork.co.uk/aggregates/?region=60008494&types=2268,2305,2267,2288,2287,2307,2272,2309,2073,2310,2270,2306,2286,2311,2308,2393,2396,3779,2401,2390,2397,2392,3683,2389,2399,2395,2398,9828,2400,3645,2329,3828,9836,9832,44,3693,15317,3725,3689,2327,9842,2463,2317,2321,3695,9830,3697,9838,2312,3691,2319,9840,3775,2328,2358,2345,2344,2367,17392,2348,9834,2366,2361,17898,2360,2354,2352,9846,9848,2351,2349,2346,12836,17136,28974,2867,2868,2869,2870,2871,2872,2875,2876")
