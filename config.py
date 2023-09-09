c9f2787484d2c3fc4c27d4dfed8a4dfe88b46736
DDSNA <9444147+DDSNA@users.noreply.github.com>
2023-09-09 03:19:53 +0300

    small variables fix 2

--- config.py
+++ config.py
@@ -5,9 +5,9 @@ class Config:
     DEBUG = False
     DEVELOPMENT = False
     SECRET_KEY = os.getenv("SECRET_KEY", "this-is-the-default-key")
-    HIST_KEY = os.getenv("HIST_KEY", "no-hist-key")
+    UP_KEY = os.getenv("UP_KEY", "no-hist-key")
     FRONTEND_KEY = os.getenv("FRONTEND_KEY", "no-frontend-key")
-    HIST_DB = os.getenv("HIST_DB", "no-hist-db")
+    UP_DB = os.getenv("UP_DB", "no-hist-db")
     FRONTEND_DB = os.getenv("FRONTEND_DB", "no-frontend-db")
 class ProductionConfig(Config):
     pass
